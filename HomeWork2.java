4bdb3abbb9370abb82527f6d8fd552af8ed390e9
vankhao <33309463+vankhao@users.noreply.github.com>
2017-11-28 19:36:06 +0300

    Add files via upload

---
+++ HomeWork2.java
@@ -0,0 +1,69 @@
+package by.itacademy.HomeWork.HomeWork2;
+
+import org.omg.Messaging.SYNC_WITH_TRANSPORT;
+import org.omg.PortableInterceptor.SYSTEM_EXCEPTION;
+
+import java.util.Arrays;
+
+public class HomeWork2 {
+//     The first task
+    public static void main(String args[]){
+        int a[] = {2,11,10,9,6,1,7,8,9,16};
+        int max = a[0];
+        int min = a[0];
+        for(int i=0; i<a.length; i++){
+            if(a[i]> max)
+                max=a[i];}
+        for(int i=0; i<a.length; i++){
+            if(a[i]< min)
+                min=a[i];}
+        System.out.println("max value =" + " " + max);
+        System.out.println("min value =" + " " + min);
+    }
+
+
+
+     //The second task
+    public static void getValue(String args[]) {
+        float newFloat[] = {2.1f, 2.1f, 10.9f, 1.5f, 6.2f, 1.5f, 7.1f, 8.6f, 9.5f, 2.1f};
+        float sameValue1 = newFloat[0];
+        float sameValue2 = newFloat[0];
+        for (float i = 0; i < newFloat.length; i++) {
+            if (sameValue1 == sameValue2) {
+
+            }
+
+        }
+    }
+
+
+     // The third task
+    public static void getList(String [] args){
+        int[] listNumber = {1,2,3,4,5,6};
+        int a = listNumber[0];
+        for(int i = 0; i < listNumber.length; i++){
+            System.out.println( "list: " + i );}
+        for(int i = 0; i < listNumber.length; i++){
+            listNumber[i] = (int)(Math.random()*100);
+            System.out.println(listNumber[i] + " ");
+        }
+    }
+
+
+
+        // The forth task
+
+
+        // The fifth task
+            public static void getMean(String[] args) {
+
+                int[] a = {9, 8, 7, 2, 3, 4, 1, 0, 6, 5};
+                Arrays.sort(a);
+                for(int i = 0; i < a.length; i ++) {
+                    System.out.print(a[i] + " ");
+        }
+    }
+}
+
+
+
